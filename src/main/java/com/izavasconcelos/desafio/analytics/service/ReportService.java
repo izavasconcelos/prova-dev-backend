d606cf87ec73e5c18aa420c3bc7f9e92485aade8
izavasconcelos <vasconcelos.izas@gmail.com>
2020-11-21 16:20:19 -0300

    refactor ReportService

--- src/main/java/com/izavasconcelos/desafio/analytics/service/ReportService.java
+++ src/main/java/com/izavasconcelos/desafio/analytics/service/ReportService.java
@@ -3,11 +3,12 @@ package com.izavasconcelos.desafio.analytics.service;
 
 import com.izavasconcelos.desafio.analytics.controller.DataController;
 import com.izavasconcelos.desafio.analytics.model.Report;
-import com.izavasconcelos.desafio.analytics.model.Sales;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
@@ -20,16 +21,18 @@ public class ReportService {
     }
 
     public String reportDataAnalysis() {
-        List<Report> report= new ArrayList<>();
-        report.add(new Report(String.valueOf(amountOfCustomers()),
+        Report report = new Report(String.valueOf(amountOfCustomers()),
                 String.valueOf(amountOfSalesman()),
                 expensiveSaleId(),
-                getWorstSalesman()));
+                getWorstSalesman());
         return report.toString();
     }
 
     public String expensiveSaleId() {
-        Map<String, Double> totalSales = dataController.getTotalSales();
+        Map<String, Double> totalSales = dataController.getTotalSalesById();
+        if(totalSales.isEmpty()){
+            return "none";
+        }
         totalSales = totalSales.entrySet().stream()
                 .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
                 .collect(Collectors.toMap(
@@ -38,6 +41,7 @@ public class ReportService {
                         (x,y)-> {throw new AssertionError();}, LinkedHashMap::new));
 
         return totalSales.keySet().stream().findFirst().get();
+
     }
 
     public int amountOfCustomers() {
@@ -48,13 +52,12 @@ public class ReportService {
         return dataController.getSalesmanList().size();
     }
 
-    public List<Sales> getSalesList() {
-        return dataController.getSalesList();
-    }
-
     public String getWorstSalesman() {
-        Map<String, Double> salesmanTotalSales = dataController.getTotalSalesman();
+        Map<String, Double> salesmanTotalSales = dataController.getTotalSalesBySalesman();
 
+        if(salesmanTotalSales.isEmpty()) {
+            return "none";
+        }
         salesmanTotalSales = salesmanTotalSales.entrySet().stream()
                 .sorted(Map.Entry.comparingByValue(Comparator.naturalOrder()))
                 .collect(Collectors.toMap(
